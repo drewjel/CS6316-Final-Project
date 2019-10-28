c4a8edf0b3d6d3f71d85289357b0f4b2afd0b33d
drewjel <andrew.james.elsey@gmail.com>
2019-10-28 15:34:22 -0400

    added main and model file

---
+++ main_file.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 28, 2019
+
+@author: andrewelsey
+'''
+
+import numpy as np
+import tensorflow as tf
+
+if __name__ == '__main__':
+    pass
---
+++ on_lstm_model.py
@@ -0,0 +1,21 @@
+'''
+Created on Oct 28, 2019
+
+@author: andrewelsey
+'''
+
+import numpy as np
+import tensorflow as tf
+
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
